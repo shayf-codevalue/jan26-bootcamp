9289213e50b7c03e257a6d11d860e2887d04d72e
Sara Hamoud <Sarahamoud2310@gmail.com>
2026-02-02 13:31:09 +0200

    the code have been solved

--- prime-func.py
+++ prime-func.py
@@ -1,7 +1,7 @@
-def x(n):
+def prime_number(n):
     for num in range(2, n + 1):
         is_prime = True
-        for i in range(1, num+1):
+        for i in range(1, num):
             if num % i == 0:
                 is_prime = False
                 break

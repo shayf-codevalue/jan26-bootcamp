33fcd669a13aee13093b406e29cf0693982f7f95
mariana <marianashannan@gmail.com>
2026-02-02 11:33:44 +0200

    added a file

---
+++ bootcamp1.py
@@ -0,0 +1,10 @@
+def prime_num(n):
+  for num in range (2,n+1):
+    is_prime=True
+    for i in range(2 , int(num*0.5+1)):
+      if num%i == 0:
+        is_prime=False
+        break
+    if is_prime:
+      print(num)
+prime_num(20)

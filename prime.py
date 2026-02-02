672d1a68ef798ca38f677fa00032844ad793be07
ameerabo <ameerabo11a@gmail.com>
2026-02-02 11:30:17 +0200

    Update prime.py
    Update prime.py
    
    Co-authored-by: danialtrody <141449225+danialtrody@users.noreply.github.com>

--- prime.py
+++ prime.py
@@ -1,6 +1,6 @@
 def print_primes_up_to_n(n):
     for num in range(2, n + 1):
-        is_prime = False
+        is_prime = True
         for i in range(2, int(num ** 0.5) + 1):
             if num % i == 0:
                 is_prime = False

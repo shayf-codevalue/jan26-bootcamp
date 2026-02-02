90eb2e5e1d7b35cef75e8c02e4d0200d888cfb33
Lama <lama.abo248@gmail.com>
2026-02-02 11:51:33 +0200

    9

--- aaa.py
+++ aaa.py
@@ -1,9 +1,10 @@
 def primenumber(N):
 {
     for i in range(2, N + 1):
-        Flag=True
+        Prime=True
         for j in range(2, i):
           if i % j == 0:
-           Flag = False	
-        if Flag : print(i) 
+           Prime = False
+	   break
+        if Prime : print(i) 
 }

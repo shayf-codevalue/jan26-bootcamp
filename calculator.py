c3e8b6c59efa6204ccab42d3878ed83ae29abfd4
ofirshuali1609 <115349516+ofirshuali1609@users.noreply.github.com>
2026-02-03 09:52:30 +0200

    ofir calculator fix bugs

--- calculator.py
+++ calculator.py
@@ -2,17 +2,19 @@ import sys
  
 operation = sys.argv[1]
 num1 = sys.argv[2]
+num1 = int(num1)
 num2 = sys.argv[3]
+num2 = int(num2)
  
 def calculate(op, a, b):
     if op == 'add':
-        # TODO: RETURN HERE
+        return a+b
     if op == 'subtract':
-        # TODO: RETURN HERE
+        return a-b
     if op == 'multiply':
-        # TODO: RETURN HERE
-    if op == 'divide':
-        # TODO: RETURN HERE
+        return a*b
+    if op == 'divide' and b != 0:
+        return a/b
     return 'Unknown operation'
  
 result = calculate(operation, num1, num2)

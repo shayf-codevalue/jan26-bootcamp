4db9b2eb4b266b5505c13ce4ad7f460cebb89696
Daniella Kadmany <your_email@gmail.com>
2026-02-03 09:48:11 +0200

    fix our codesd

---
+++ workshop-project/app.js
@@ -0,0 +1,11 @@
+// TASK: Use Ctrl+D to fix the misspelled variable 'user_name'
+function greetUser() {
+    const user_name = "Junior Dev";
+    console.log("Welcome to the team, " + user_name)
+
+    console.log("Access granted for: " + user_name);
+}
+
+// TASK: Use Alt + Up/Down to move this line inside the function above
+
+greetUser();
